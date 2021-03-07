58d4eb0cdae6e943cda4f6f3dc3c26b0eeb39cae
kamijohfn <76580010+kamijohfn@users.noreply.github.com>
2021-03-07 11:57:22 +0900

    商品の登録

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,12 +1,26 @@
 class ItemsController < ApplicationController
 
   def index
+    @items = Item.order("created_at DESC")
   end
 
   def new
+    @item = Item.new
   end
 
   def create
+    @item = Items.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
   
+  private
+  def item_params
+    params.require(:item).permit(:name, :description, :category_id, :status_id, :delivery_fee_id, :area_id, :day_id, :image).merge(user_id:current_user.id)
+  end
+
+
 end
