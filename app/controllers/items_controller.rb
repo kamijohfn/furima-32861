c0369970562a3b0ee0768d6f9837fb6c6f63fc6f
kamijohfn <76580010+kamijohfn@users.noreply.github.com>
2021-03-10 09:11:58 +0900

    エラー修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,7 +9,7 @@ class ItemsController < ApplicationController
   end
 
   def create
-    @item = Items.new(item_params)
+    @item = Item.new(item_params)
     if @item.save
       redirect_to root_path
     else
@@ -19,7 +19,7 @@ class ItemsController < ApplicationController
   
   private
   def item_params
-    params.require(:item).permit(:name, :description, :category_id, :status_id, :delivery_fee_id, :area_id, :day_id, :image).merge(user_id:current_user.id)
+    params.require(:item).permit(:name, :description, :category_id, :status_id, :delivery_fee_id, :area_id, :day_id, :price, :image).merge(user_id: current_user.id)
   end
 
 
