768b2958978cd1aaeb878a763c7cca7bebf0a05e
kamijohfn <76580010+kamijohfn@users.noreply.github.com>
2021-03-12 19:07:27 +0900

    商品出品機能の修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -42,10 +42,22 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include("Category can't be blank", "Category is not a number")
       end
 
+      it 'カテゴリーの情報が空の時、出品できない'do
+        @item.category_id  = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category must be other than 1")
+      end
+
       it '商品の状態の情報がないと出品できない' do
         @item.status_id = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Status can't be blank", "Status is not a number")
+      end
+
+      it '商品の状態の情報が空の時、出品できない'do
+        @item.status_id  = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Status must be other than 1")
       end
 
       it '配送料の負担の情報がない出品できない' do
@@ -54,10 +66,22 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include("Delivery fee can't be blank", "Delivery fee is not a number")
       end
 
+      it '配送料の負担の情報が空の時、出品できない'do
+        @item.delivery_fee_id  = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Delivery fee must be other than 1")
+      end
+
       it '発送元の地域についての情報がないと出品できない' do
         @item.area_id  = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Area can't be blank", "Area is not a number")
+      end
+
+      it '発送元の地域が空の時、出品できない'do
+        @item.area_id  = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Area must be other than 1")
       end
 
       it '発送までの日数についての情報がないと出品できない' do
@@ -66,13 +90,25 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include("Day can't be blank", "Day is not a number")
       end
 
+      it '発送までの日数が空の時、出品できない'do
+      @item.day_id  = '1'
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Day must be other than 1")
+      end
+
       it '価格についての情報がないと出品できない' do
         @item.price   = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Price can't be blank", "Price is invalid", "Price is not a number")
       end
 
-      it '価格の範囲が、¥300~¥9,999,999の間であること' do
+      it '価格が299円以上では登録できないこと' do
+        @item.price = 100
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
+      end
+
+      it '価格が¥9,999,999以下であること' do
         @item.price = 10000000
         @item.valid?
         expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
@@ -80,6 +116,18 @@ RSpec.describe Item, type: :model do
 
       it '販売価格は半角数字のみ保存可能であること' do
         @item.price = '１００００'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number")
+      end
+
+      it '半角英数混合では登録できない' do
+        @item.price = '１００ek'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number")
+      end
+
+      it '半角英語だけでは登録できない' do
+        @item.price = 'aaaaaa'
         @item.valid?
         expect(@item.errors.full_messages).to include("Price is not a number")
       end
