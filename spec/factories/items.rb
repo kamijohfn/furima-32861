28ca5962b3871d87bd4945e016edc5f085e95dca
kamijohfn <76580010+kamijohfn@users.noreply.github.com>
2021-03-12 12:50:38 +0900

    商品出品機能　単体テストコード

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,18 @@
 FactoryBot.define do
   factory :item do
+    name               {Faker::Commerce.product_name}
+    description        {Faker::Lorem.sentence}
+    category_id        {Faker::Number.within(range: 2..11)}
+    status_id          {Faker::Number.within(range: 2..7)}
+    delivery_fee_id    {Faker::Number.within(range: 2..3)}
+    area_id            {Faker::Number.within(range: 2..48)}
+    day_id             {Faker::Number.within(range: 2..4)}
+    price              {Faker::Number.number(digits: 4)}
+    association :user
     
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'),filename: 'test_image.png')
+    end
+
   end
 end
