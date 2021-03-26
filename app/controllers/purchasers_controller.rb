0e61d4fb8f591aa12f910fad34ee7d9252c7ae7f
kamijohfn <76580010+kamijohfn@users.noreply.github.com>
2021-03-26 17:55:11 +0900

    クレジットカード決済機能

--- app/controllers/purchasers_controller.rb
+++ app/controllers/purchasers_controller.rb
@@ -6,6 +6,13 @@ class PurchasersController < ApplicationController
   def create
     @purchaser_delivery = PurchaserDelivery.new(purchaser_params)
     if  @purchaser_delivery.valid? 
+        binding.pry
+        Payjp.api_key = ENV["PAYJP_SECRET_KEY"] 
+        Payjp::Charge.create(
+        amount: Item.find(params[:item_id]).price,
+        card: purchaser_params[:token], 
+        currency: 'jpy'
+      )
         @purchaser_delivery.save
         redirect_to root_path
     else
@@ -15,7 +22,7 @@ class PurchasersController < ApplicationController
 
   private
   def purchaser_params
-    params.permit(:postal_code, :area_id, :municipality, :address, :building_name, :phone_number)
+    params.require(:purchaser_delivery).permit(:postal_code, :area_id, :municipality, :address, :building_name, :phone_number).merge(token: params[:token], user_id: current_user.id, item_id: params[:item_id])
   end
 
 
