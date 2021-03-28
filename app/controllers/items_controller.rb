d55692b41f7b22de91d1a456f5571d653871298b
kamijohfn <76580010+kamijohfn@users.noreply.github.com>
2021-03-28 22:44:02 +0900

    商品購入機能修正３

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_item, only: [:edit, :show, :update, :destroy]
   before_action :move_to_root_path, only: [:edit, :update, :destroy]
-  before_action :sold_root_path, only: :show
+  before_action :sold_root_path, only: :edit
 
   def index
     @items = Item.all.order("created_at DESC")
